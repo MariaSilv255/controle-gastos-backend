2e46bbbbfc2338c6511dfbe7d06317276382025f
Maria Aparecida <maria.info108@gmail.com>
2026-01-08 13:44:36 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/maria/controle_gastos_backend/ControleGastosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.maria.controle_gastos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleGastosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleGastosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maria/controle_gastos_backend/ControleGastosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maria.controle_gastos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleGastosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
